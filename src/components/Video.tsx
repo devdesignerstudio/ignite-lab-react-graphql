d3b63cf48369c1f76e0301e3709909da41e5442e
Célio devdesignerstudio <devdesignerstudio@protonmail.com>
2022-06-27 23:24:28 -0300

    alteração hyperlinks

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -88,11 +88,17 @@ export function Video(props: VideoProps){
 
             </div>
             <div className="flex flex-col gap-4">
-              <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+              <a 
+              target="blank" 
+              href="https://discord-service.rocketseat.dev/signin/ignite-lab" 
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
                 <DiscordLogo size={24} />
                 Comunidade do Discord
               </a>
-              <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+              <a
+              target="blank"  
+              href="https://evento.rocketseat.com.br/ignite-lab/desafios/checkpoints" 
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
                 <Lightning size={24} />
                 Acesse o Desafio
               </a>
@@ -100,7 +106,10 @@ export function Video(props: VideoProps){
 
         </div>
         <div className="gap-8 mt-20 grid grid-cols-2">
-            <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <a 
+            target="blank" 
+            href="https://efficient-sloth-d85.notion.site/Material-complementar-86d4ef35af16471ebc3ae3eba1a378e5" 
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
               <div className="bg-green-700 h-full p-6 flex items-center">
                 <FileArrowDown size={40} />
               </div>
@@ -114,7 +123,9 @@ export function Video(props: VideoProps){
                 <CaretRight size={24} />
               </div>
             </a>
-            <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <a target="blank" 
+            href="https://drive.google.com/drive/folders/1mxWnvlqmH7MbVRv2Na9xFNgCQCygM1iR?usp=sharing" 
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
               <div className="bg-green-700 h-full p-6 flex items-center">
                 <FileArrowDown size={40} />
               </div>
